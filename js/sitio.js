203896f2dcd9904557e2e63f92ea78b984a1b091
jesus-webcorp <79873404+jesus-webcorp@users.noreply.github.com>
2023-02-12 22:09:31 -0600

    slick como slider para clientes

--- js/sitio.js
+++ js/sitio.js
@@ -5,155 +5,90 @@ $(function () {
     /*     $('#menu-principal .navbar-toggler').click(function () {
      */
     $('.boton-menu').toggleClass('icono-cerrar');
-});
 
-/*Al hacer click en un enlace del menú principal */
-$('#menu-principal .navbar-nav a').click(function () {
-    /* 1) Quita la clase ".icono-cerrar" */
-    $('.boton-menu').removeClass('icono-cerrar');
 
-    /*2) Contraemos el menu */
-    $('#menu-principal .navbar-collapse').collapse('hide');
-});
+    /*Al hacer click en un enlace del menú principal */
+    $('#menu-principal .navbar-nav a').click(function () {
+        /* 1) Quita la clase ".icono-cerrar" */
+        $('.boton-menu').removeClass('icono-cerrar');
 
-/*------------------------
-INICIAMOS WOW
--------------------------*/
-new WOW().init();
+        /*2) Contraemos el menu */
+        $('#menu-principal .navbar-collapse').collapse('hide');
+    });
 
-/*----------------------------------
-Iniciamos smoothScroll (Scroll Suave)
---------------------------------*/
-smoothScroll.init({
-    speed: 1000, // Integer. How fast to complete the scroll in milliseconds
-    offset: 100, // Integer. How far to offset the scrolling anchor location in pixels
 
-});
+    /*------------------------
+    INICILIZANDO WOW
+    -------------------------*/
+    new WOW().init();
 
-/*-----------------------------------------------------------
-2. INICIANDO "page-scroll-to-id"
--------------------------------------------------------------*/
-$('#menu-principal a').mPageScroll2id({
-    offset: 100,
-    highlightClass: 'active'
-});
+    /*----------------------------------
+    Iniciamos smoothScroll (Scroll Suave)
+    --------------------------------*/
+    smoothScroll.init({
+        speed: 1000, // Integer. How fast to complete the scroll in milliseconds
+        offset: 100, // Integer. How far to offset the scrolling anchor location in pixels
 
-/*-----------------------------------------------------------
-3. CONFIGURACION DE SWIPER PARA LA PARTE DEL BANNER
-------------------------------------------------------------*/
-const swiper = new Swiper('#animacion', {
-    /*Botones de navegación */
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    /*Botones de paginación */
-    pagination: {
-        el: '.swiper-pagination',
-        type: 'bullets',
-        clickable: true,
-    },
-    speed: 500,
-    effect: 'fade',
-    grabCursor: true,
-    loop: true,
-    //   autoplay: {
-    //     delay: 10000,            
-    //   },
-    keyboard: {
-        enabled: true,
-        onlyInViewport: true,
-    }
-});
+    });
 
-/*-----------------------------------------------------------
-4. INICIANDO jquery.counterup
--------------------------------------------------------------*/
-$('.counter').counterUp({
-delay: 10,
-time: 2000
-});
-});
+    /*---------------------------------
+       CABECERA ANIMADA
+     ----------------------------------*/
+    $(window).scroll(function () {
+        var nav = $('.encabezado');
+        var scroll = $(window).scrollTop();
 
-/*-----------------------------------------------------------
-5. IMAGEN DE USUARIO DE DONACION DE SANGRE
-------------------------------------------------------------*/
-$(document).on('change', '#file-user', function () {
-    var filesCount = $(this)[0].files.length;
-    var textbox = $(this).prev();
-    var ext = $(this).val().split('.').pop();
-    var archivo = document.getElementById("file-user").files[0];
-
-    if (ext == "png" || "jpg") {
-        if (filesCount === 1) {
-
-            var reader = new FileReader();
-            reader.readAsDataURL(archivo);
-            var fileName = $(this).val().split('\\').pop();
-            textbox.text(fileName);
-            reader.onloadend = function () {
-                document.getElementById("img").src = reader.result;
-            }
+        if (scroll >= 80) {
+            nav.addClass("fondo-menu");
         } else {
-            textbox.text(filesCount + ' files selected');
+            nav.removeClass("fondo-menu");
         }
+    });
 
-    } else {
-        $(this).val('');
-        Toastify({
-            text: "El archivo debe ser una imagen",
-            duration: 3000,
-            className: "info",
-            // avatar: "../../assets/img/logop.png",
-            style: {
-                background: "linear-gradient(to right, red, orange)",
+
+    /*-----------------------------------------------------------
+    2. CONFIGURACION DE SLICK PARA LA PARTE DE ALGUNOS DE NUESTROS CLIENTES
+    ------------------------------------------------------------*/
+    $('.clientes').slick({
+        dots: true,
+        infinite: true,
+        speed: 600,
+        arrows: true,
+        slidesToShow: 8,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 700,
+        responsive: [
+            {
+                //Vista lg >=992px
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 6,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
             },
-            offset: {
-                x: 50, // horizontal axis - can be a number or a string indicating unity. eg: '2em'
-                y: 90 // vertical axis - can be a number or a string indicating unity. eg: '2em'
+            {
+                //Vista md >=768px
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
             },
-        }).showToast();
-    }
-});
-
-/*-----------------------------------------------------------
-6. IMAGEN DE LA CAMPAÑA
-------------------------------------------------------------*/
-$(document).on('change', '#file-campaña', function () {
-    //alert("Dentro");
-    var filesCount = $(this)[0].files.length;
-    var textbox = $(this).prev();
-    var ext = $(this).val().split('.').pop();
-    var archivo = document.getElementById("file-campaña").files[0];
-
-    if (ext == "png" || "jpg") {
-        if (filesCount === 1) {
-
-            var reader = new FileReader();
-            reader.readAsDataURL(archivo);
-            var fileName = $(this).val().split('\\').pop();
-            textbox.text(fileName);
-            reader.onloadend = function () {
-                document.getElementById("img").src = reader.result;
+            {
+                //Vista sm >=576px
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
             }
-        } else {
-            textbox.text(filesCount + ' files selected');
-        }
-
-    } else {
-        $(this).val('');
-        Toastify({
-            text: "El archivo debe ser una imagen",
-            duration: 3000,
-            className: "info",
-            // avatar: "../../assets/img/logop.png",
-            style: {
-                background: "linear-gradient(to right, red, orange)",
-            },
-            offset: {
-                x: 50, // horizontal axis - can be a number or a string indicating unity. eg: '2em'
-                y: 90 // vertical axis - can be a number or a string indicating unity. eg: '2em'
-            },
-        }).showToast();
-    }
+        ]
+    });
 });
